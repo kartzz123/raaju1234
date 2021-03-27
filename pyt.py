ad615c88228ba1a3a7e34e4e50112a2b88a024ba
kartzz123 <kartzz282@gmail.com>
2021-03-27 18:09:28 +0530

    my commit to the branch

---
+++ pyt.py
@@ -0,0 +1,16 @@
+calculate_of_units = 50
+name_unit ="hours"
+
+def days_to_unit (num_of_days):
+    print ( f"{num_of_days} days are {num_of_days * calculate_of_units}" )
+    print ("hello virtusa amigo")
+
+
+
+
+days_to_unit(20)
+days_to_unit(40)
+days_to_unit(60)
+days_to_unit(90)
+
+
